6040c4314bd99c57d1c34d6f5a0a77753e105d4a
Hyein <ms8370@naver.com>
2020-05-12 21:59:48 +0900

    trigger추가

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(UnityEngine.Collider other)
+    {
+        Debug.Log("왼쪽 구역 진입");
+    }
+
+    void OnTriggerExit(UnityEngine.Collider other)
+    {
+        Debug.Log("왼쪽 구역 나감");
+    }
+
+}

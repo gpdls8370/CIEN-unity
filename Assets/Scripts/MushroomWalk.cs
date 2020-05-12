f0e320700c2b0a4c0bede25d93d8ce9c81b62d84
Hyein <ms8370@naver.com>
2020-05-12 21:45:40 +0900

    유니티 파일 업로드

---
+++ Assets/Scripts/MushroomWalk.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushroomWalk : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(-0.01f * speed, 0, 0, Space.World);
+
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(0, 0, 0.01f * speed, Space.World);
+
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(0.01f * speed, 0, 0, Space.World);
+
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(0, 0, -0.01f * speed, Space.World);
+
+
+        if (Input.GetKey(KeyCode.Space))
+            transform.Translate(0, 0.2f, 0, Space.World);
+    }
+}
